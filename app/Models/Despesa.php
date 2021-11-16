2c91b2e162b2ae81434974a0286a04c227cbdf77
Wagner Silva <wagnercodeandgo@gmail.com>
2021-11-16 12:35:25 -0300

    metodo edit do banco

--- app/Models/Despesa.php
+++ app/Models/Despesa.php
@@ -70,16 +70,28 @@ class Despesa extends Model
         return $fornecedor;
     }
 
-    static function set($fornecedor)
-    {
-        DB::update("UPDATE intranet.tab_fornecedor
-        SET de_razao_social = ?, inscricao_estadual = ?, dt_inicio = ?, de_nome_fantasia = ?
-        WHERE id_fornecedor = ?", [
-            $fornecedor->de_razao_social,
-            $fornecedor->inscricao_estadual,
-            $fornecedor->dt_inicio,
-            $fornecedor->de_nome_fantasia,
-            $fornecedor->id_fornecedor
+
+    static function set($despesa){
+        DB::update( "UPDATE intranet.tab_despesa
+        SET fk_tab_centro_custo_id = ?, fk_tab_empresa_id = ?, fk_tab_tipo_despesa_id = ?, 
+            numero_despesa = ?, qt_parcelas_despesa = ?, serie_despesa = ?, 
+            dt_emissao = ?, valor_total_despesa = ?, fk_status_despesa_id = ?,
+            fk_tab_fornecedor_id = ?, fk_tab_empregado_id = ? , dt_inicio = ? 
+        WHERE id_despesa = ?", [
+                
+            $despesa->fk_tab_centro_custo_id,
+            $despesa->fk_tab_empresa_id,
+            $despesa->fk_tab_tipo_despesa_id,
+            $despesa->numero_despesa,
+            $despesa->qt_parcelas_despesa,
+            $despesa->serie_despesa,
+            $despesa->dt_emissao,
+            $despesa->valor_total_despesa,
+            $despesa->fk_status_despesa_id,
+            $despesa->fk_tab_fornecedor_id,
+            $despesa->fk_tab_empregado_id,
+            $despesa->dt_inicio, 
+            $despesa->id_despesa 
         ]);
     }
 
