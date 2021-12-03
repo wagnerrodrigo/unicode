04a265697b5774c7a0b4fa6c3dcd42152f37b3e6
Wagner Silva <wagnercodeandgo@gmail.com>
2021-12-03 18:21:09 -0300

    Ajax de busca das informacoes da tela pagamento

--- public/assets/js/custom-js/pagamento.js
+++ public/assets/js/custom-js/pagamento.js
@@ -3,21 +3,140 @@ $("#inst_banco").keyup(function() {
 
     if (instBanco != "") {
         $.ajax({
-            url: `/lancamentos/info-conta/${instBanco}`,
             type: "GET",
+            url: `/pagamento/info-conta/${instBanco}`,
             dataType: "json",
         }).done(function(response) {
-            // mostra os resultado do nome do banco e coloca em um div
             $("#Resultado_inst_banco").html("");
+            // mostra os resultado do nome do banco e coloca em um div
             $.each(response, function(key, val) {
                 $("#Resultado_inst_banco").append(
-                    `<div class="item" value="${val.fk_tab_inst_banco_id}" >${val.de_banco} </div>`
+                    `<div class="item" value="${val.id}" >${val.de_banco} </div>`
                 );
             });
             $(".item").click(function() {
                 $("#inst_banco").val($(this).text());
+                $("#Resultado_inst_banco").html("");
+                var id = $(this).attr("value");
 
-            })
+                //busca as instituições bancarias relacionados com a agencia e mostra no select
+                $("#id_inst_banco").attr("value", id);
+                $("#Resultado_inst_banco").html("");
+                // a cada nova requisição, limpa o option do select
+                $("#agencia").html("");
+                $.ajax({
+                        type: "GET",
+                        url: `/pagamento/info-agencia/${id}`,
+                        dataType: "json",
+                    })
+                    .done(function(response) {
+                        //mostra os resultados da busca em um select
+                        $.each(response, function(key, val) {
+                            $("#agencia").append(
+                                `<option selectd class="item" value="${val.nu_agencia}" >${
+                                val.nu_agencia !== ""
+                                ? val.nu_agencia 
+                                : " ---- Número da agência não cadastrado ----"
+                            }</option>`
+                                // Se a instuição bancaria não possuir um numero de agencia cadastrada informa a mensagem
+                            );
+                        });
+
+                    });
+                $("#conta_banco").html("")
+                    // a cada nova requisição, limpa o option do select
+                $.ajax({
+                    //mostra os resultados da busca em um select
+                    type: "GET",
+                    url: `/pagamento/info-contaBancaria/${id}`,
+                    dataType: "json",
+                }).done(function(response) {
+                    $.each(response, function(key, val) {
+                        $("#conta_banco").append(
+                            `<option selectd class="item" value="${val.nu_conta}" >${
+                            val.nu_conta !== ""
+                            ? val.nu_conta 
+                            : " ---- Número da Conta Bancaria não cadastrado ----"
+                        }</option>`
+                            // Se a agência não possuir um numero de Conta bancaria cadastrada informa a mensagem
+                        );
+                    })
+                })
+            });
+        });
+    }
+});
+
+
+
+
+
+
+$("#inst_banco_forne_empr").keyup(function() {
+    var inst_banco_forne_empr = $(this).val();
+
+    if (inst_banco_forne_empr != "") {
+        $.ajax({
+            type: "GET",
+            url: `/pagamento/info-conta/${inst_banco_forne_empr}`,
+            dataType: "json",
+        }).done(function(response) {
+            $("#Resultado_inst_banco_forn_empr").html("");
+            // mostra os resultado do nome do banco e coloca em um div
+            $.each(response, function(key, val) {
+                $("#Resultado_inst_banco_forn_empr").append(
+                    `<div class="item" value="${val.id}" >${val.de_banco} </div>`
+                );
+            });
+            $(".item").click(function() {
+                $("#inst_banco_forne_empr").val($(this).text());
+                $("#Resultado_inst_banco_forn_empr").html("");
+                var id = $(this).attr("value");
+
+                //busca as instituições bancarias relacionados com a agencia e mostra no select
+                $("#inst_banco_forne_empr").attr("value", id);
+                $("#Resultado_inst_banco_forn_empr").html("");
+                // a cada nova requisição, limpa o option do select
+                $("#agencia_forne_empr").html("");
+                $.ajax({
+                        type: "GET",
+                        url: `/pagamento/info-agencia/${id}`,
+                        dataType: "json",
+                    })
+                    .done(function(response) {
+                        //mostra os resultados da busca em um select
+                        $.each(response, function(key, val) {
+                            $("#agencia_forne_empr").append(
+                                `<option selectd class="item" value="${val.nu_agencia}" >${
+                                val.nu_agencia !== ""
+                                ? val.nu_agencia 
+                                : " ---- Número da agência não cadastrado ----"
+                            }</option>`
+                                // Se a instuição bancaria não possuir um numero de agencia cadastrada informa a mensagem
+                            );
+                        });
+
+                    });
+                $("#conta_banco_forne_empr").html("")
+                    // a cada nova requisição, limpa o option do select
+                $.ajax({
+                    //mostra os resultados da busca em um select
+                    type: "GET",
+                    url: `/pagamento/info-contaBancaria/${id}`,
+                    dataType: "json",
+                }).done(function(response) {
+                    $.each(response, function(key, val) {
+                        $("#conta_banco_forne_empr").append(
+                            `<option selectd class="item" value="${val.nu_conta}" >${
+                            val.nu_conta !== ""
+                            ? val.nu_conta 
+                            : " ---- Número da Conta Bancaria não cadastrado ----"
+                        }</option>`
+                            // Se a agência não possuir um numero de Conta bancaria cadastrada informa a mensagem
+                        );
+                    })
+                })
+            });
         });
     }
 });
