3059b0a1bcbbf5da15ae10c0ed98e1e36ebea3e9
Wagner Silva <wagnercodeandgo@gmail.com>
2021-12-03 18:19:26 -0300

    Adicionado Metodos no Controller e Model
    -Metodos de busca no controller
      -showDataInsBanc busca instituições bancarias
    
      -ShowDataAgency  busca as agências das instituições bancarias selecionada anterioment
    
      -ShowBankAccount  busca as cantas bancarias das agências selecionada anterioment
    
    -Modell
    
    - showInfoAccount sql de busca das instituições bancarias
    
    - showInfoAgency sql de busca das agências
    
    -showInfoBankAccount sql de busca das contas bancarias

--- app/Models/Pagamento.php
+++ app/Models/Pagamento.php
@@ -23,5 +23,37 @@ class Pagamento extends Model
     
     }
 
+    static function showInfoAccount($info){
+        $query = ("SELECT
+                 id, co_banco, de_banco
+                 FROM intranet.intranet.tab_inst_banco
+                 WHERE de_banco LIKE upper('%".$info."%')");
+
+        $pagamento = DB::select($query);
+        return $pagamento;
+    }
+
+    static function showInfoAgency($id)
+    {
+        $query = DB::select("SELECT conta_banco.fk_tab_inst_banco_id,
+                   conta_banco.nu_agencia
+                   fROM intranet.tab_conta_bancaria AS conta_banco
+                   JOIN intranet.tab_inst_banco AS ins_bank ON(ins_bank.id = conta_banco.fk_tab_inst_banco_id)
+                   WHERE ins_bank.id = ?;", [$id]);
+       
+        return $query;
+    }
+
+    static function showInfoBankAccount($id)
+    {
+        $query = DB::select("SELECT 
+                    conta_banco.fk_tab_inst_banco_id,
+                    conta_banco.nu_conta
+                    FROM intranet.tab_conta_bancaria AS conta_banco
+                    JOIN intranet.tab_inst_banco AS ins_bank ON(ins_bank.id = conta_banco.fk_tab_inst_banco_id)
+                    WHERE ins_bank.id = ?", [$id]);
+
+        return $query;
+    }
     
 }
