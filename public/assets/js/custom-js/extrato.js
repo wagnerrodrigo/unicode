ed03f4d11e6f9bd1d63b553454ef2eee735f9ff4
Wagner Silva <wagnercodeandgo@gmail.com>
2021-12-13 13:59:27 -0300

    ajax busca de empresa

---
+++ public/assets/js/custom-js/extrato.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    //requisição ajax para buscar empresa
+    $.ajax({
+            type: "GET",
+            url: `http://localhost:8000/extrato/empresa`,
+            dataType: "json",
+        })
+        //caso a requisição seja bem sucedida
+        .done(function(response) {
+            $("#resultado-inputEmpresa").html("");
+            //mostra os resultados da busca em uma div
+            $.each(response, function(key, val) {
+                console.log(response);
+                $("#resultado-inputEmpresa").append(
+                    `<option class="item" value="${val.id_empresa}">${val.de_empresa}</option>`
+                );
+            });
+            //seleciona a empresa desejada
+            $(".item").click(function() {
+                $("#inputEmpresa").val($(this).text());
+                $("#resultado-inputEmpresa").html("");
+            });
+        })
+});
