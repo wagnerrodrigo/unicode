9317f09c83459d65f9dfa985f3f3cce321213a63
Wagner Silva <wagnercodeandgo@gmail.com>
2021-12-17 17:15:47 -0300

    Metodo de Pesquisa no banco

--- app/Models/Extrato.php
+++ app/Models/Extrato.php
@@ -18,4 +18,28 @@ class Extrato extends Model
         return $query;
 
     }
+
+    static function selectAll()
+    {
+        $query = DB::select(" SELECT extrato.id_extrato,
+                    extrato.fk_tab_conta_bancaria,
+                    extrato.dtend,
+                    ct_bancaria.fk_tab_inst_banco_id,
+                    ct_bancaria.nu_agencia,
+                    ct_bancaria.nu_conta,
+                    inst_banco.id,
+                    inst_banco.de_banco,
+                    empresa.id_empresa 
+                    FROM intranet.tab_extrato AS extrato 
+                    INNER JOIN intranet.tab_conta_bancaria AS ct_bancaria on (extrato.fk_tab_conta_bancaria = ct_bancaria.id_conta_bancaria)
+                    INNER JOIN intranet.tab_inst_banco AS inst_banco on (ct_bancaria.fk_tab_inst_banco_id = inst_banco.id)
+                    INNER JOIN intranet.tab_empresa AS empresa on (empresa.id_empresa = extrato.fk_empresa_id);");
+
+        $data = $query;
+
+        return $data;
+    }
+
+
+    static function findByPeriod(){}
 }
