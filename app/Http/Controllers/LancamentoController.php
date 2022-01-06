17b2096dabd633b965f68a2376cfdd2f03ef7c9f
Wagner Silva <wagnercodeandgo@gmail.com>
2022-01-06 16:58:49 -0300

    Create de rateio contas, lancamentos

--- app/Http/Controllers/LancamentoController.php
+++ app/Http/Controllers/LancamentoController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Despesa;
 use App\Models\Lancamento;
 use App\Models\Rateio;
 use Illuminate\Http\Request;
 use App\Utils\Mascaras\Mascaras;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Query\Builder;
-use Illuminate\Support\Arr;
+
+
+use function PHPUnit\Framework\isEmpty;
 
 class LancamentoController extends Controller
 {
@@ -21,19 +22,17 @@ class LancamentoController extends Controller
     public function index(Request $request)
     {
         $mascara = new Mascaras();
-        if($request->has('dt_inicio') && $request->has('dt_fim') || $request->has('status')) {
+        if ($request->has('dt_inicio') && $request->has('dt_fim') || $request->has('status')) {
             $dt_inicio_periodo = $request->input('dt_inicio');
             $dt_fim_periodo = $request->input('dt_fim');
             $status_despesa_id = $request->input('status');
 
             $lancamentos = Lancamento::selectAll($dt_inicio_periodo, $dt_fim_periodo, $status_despesa_id);
-        }else{
+        } else {
             $lancamentos = Lancamento::selectAll();
         }
 
-        //dd($lancamentos);
-
-        return view('admin.lancamentos.lista-lancamentos', compact('lancamentos' ,'mascara'));
+        return view('admin.lancamentos.lista-lancamentos', compact('lancamentos', 'mascara'));
     }
 
     /**
@@ -50,57 +49,55 @@ class LancamentoController extends Controller
      */
     public function store(Request $request)
     {
-        //
-        // dd($request->valor_rateio_pagamento);
+        
+        
 
-        $valorRateio = [];
+        if (!empty($request->valor_rateio_pagamento)) {
 
-        for($i =0; $i < count($request->valor_rateio_pagamento); $i++){
+            if ($request->valor_rateio_pagamento) {
+                for ($i = 0; $i < count($request->valor_rateio_pagamento); $i++) {
+                    $rateios[] = [
+                        'valor_rateio_pagamento' => preg_replace('/\D/', '', $request->valor_rateio_pagamento[$i]),
+                        'fk_tab_conta_bancaria' => $request->fk_tab_conta_bancaria[$i],
+                    ];
 
-            // $valorRateio[$i] = str_replace(".", "", $request->valor_rateio_pagamento[$i]);
-            // $valorRateio[$i] = str_replace(",", ".", $request->valor_rateio_pagamento[$i]);
-            $valorRateio[$i] = preg_replace('/\D/', '', $request->valor_rateio_pagamento[$i]);
-        }
-
-        if($request->valor_rateio_pagamento){
-            for($i = 0; $i < count($request->valor_rateio_pagamento); $i++){
-                $rateios[] = [
-                    'valor_rateio_pagamento' =>  trim(html_entity_decode($valorRateio[$i]), " \t\n\r\0\x0B\xC2\xA0"),
-                    'fk_tab_conta_bancaria' => $request->fk_tab_conta_bancaria[$i],
-                ];
-                // dd($valorRateio);
+                }
+                $rateio = new Rateio();
+    
+                for ($i = 0; $i < count($rateios); $i++) {
+                    $rateio->valor_rateio_pagamento = $rateios[$i]['valor_rateio_pagamento'];
+                    $rateio->fk_tab_conta_bancaria = $rateios[$i]['fk_tab_conta_bancaria'];
+                    $rateio->dt_inicio =  Carbon::now()->setTimezone('America/Sao_Paulo')->toDateTimeString();
+                    $rateio->dt_fim = null;
+                    Rateio::createRateioLancamento($rateio);
+                }
             }
-            $rateio = new Rateio();
 
-            for($i = 0; $i < count($rateios); $i++){
-                $rateio->valor_rateio_pagamento = $rateios[$i]['valor_rateio_pagamento'];
-                $rateio->fk_tab_conta_bancaria = $rateios[$i]['fk_tab_conta_bancaria'];
-                $rateio->dt_inicio =  Carbon::now()->setTimezone('America/Sao_Paulo')->toDateTimeString();
-                $rateio->dt_fim = null;
-                Rateio::createRateioLancamento($rateio);
+           
+                $lancamento = new Lancamento();
+          
+                $lancamento->id_despesa = $request->id_despesa;
+                $lancamento->fk_condicao_pagamento_id = $request->fk_condicao_pagamento_id;
+                $lancamento->dt_inicio =  Carbon::now()->setTimezone('America/Sao_Paulo')->toDateTimeString();
+                $lancamento->dt_fim = null;
+                Lancamento::create($lancamento);
+        }
+        else{
+               // INICIO requeste somente lancamento
+               $lancamento = new Lancamento();
+          
+                $lancamento->id_despesa = $request->id_despesa;
+                $lancamento->fk_condicao_pagamento_id = $request->fk_condicao_pagamento_id;
+                $lancamento->dt_inicio =  Carbon::now()->setTimezone('America/Sao_Paulo')->toDateTimeString();
+                $lancamento->dt_fim = null;
+                Lancamento::create($lancamento);
+                // FIM requeste somente lancamento
+
+               
             }
-        }
-
-
-       if($request->id_despesa){
-        for($i = 0; $i < count($request->id_despesa); $i++){
-            $lancamentos[] =[
-                'id_despesa' => $request->id_despesa[$i],
-                'fk_condicao_pagamento_id' => $request->fk_condicao_pagamento_id,
-            ];
-        }
-        $lancamento = new Lancamento();
-
-        for($i =0; $i < count($lancamentos); $i++){
-            $lancamento->id_despesa = $lancamentos[$i]['id_despesa'];
-            $lancamento->fk_condicao_pagamento_id = $lancamentos[$i]['fk_condicao_pagamento_id'];
-            $lancamento->dt_inicio =  Carbon::now()->setTimezone('America/Sao_Paulo')->toDateTimeString();
-            $lancamento->dt_fim = null;
-            Lancamento::create($lancamento);
-        }
-
-       }
-
+            
+            $despesa = new Despesa();
+            $despesa::setStatus($request->id_despesa);
 
         return  redirect()->route('lancamentos');
     }
@@ -117,19 +114,22 @@ class LancamentoController extends Controller
         return view('admin.lancamentos.detalhes-lancamento', compact('lancamento'));
     }
 
-    public function provisionamento($id){
+    public function provisionamento($id)
+    {
         $lancamento = Lancamento::findOne($id);
-        $lancamento->valor_total_despesa= Mascaras::maskMoeda($lancamento->valor_total_despesa);
-        return view('admin.lancamentos.add-lancamento',compact('lancamento'));
+        $lancamento->valor_total_despesa = Mascaras::maskMoeda($lancamento->valor_total_despesa);
+        return view('admin.lancamentos.add-lancamento', compact('lancamento'));
     }
 
 
-    public function showDataInsBanc($info){
+    public function showDataInsBanc($info)
+    {
         $lancamento = Lancamento::showInfoAccount($info);
         return response()->json($lancamento);
     }
 
-    public function showDataAgency($id){
+    public function showDataAgency($id)
+    {
         $lancamento = Lancamento::showInfoAgency($id);
         return response()->json($lancamento);
     }
@@ -139,7 +139,8 @@ class LancamentoController extends Controller
         $lancamento = Lancamento::showInfoBankAccount($id);
         return response()->json($lancamento);
     }
-    public function showProvidedEmployee($id){
+    public function showProvidedEmployee($id)
+    {
         $lancamento = Lancamento::showDataProvidedEmployee($id);
         return response()->json($lancamento);
     }
@@ -165,13 +166,6 @@ class LancamentoController extends Controller
     public function showBydateAndstatus(Request $request)
     {
 
-
-
-    // $lancamento = DB::table('intranet.tab_lancamento')->paginate(15)
-    // dd($lancamento->columns[0]->id_tab_lancamento); MUITO IMPORTANTE !!!!!
-
-    // return response()->json($lancamento);
-
         $campos = "id_tab_lancamento
         fk_condicao_pagamento_id
         dt_vencimento
@@ -183,54 +177,23 @@ class LancamentoController extends Controller
         valor_total_despesa
         dt_provisionamento
         fk_tab_lancamento_id de_pagamento";
-        // dd($campos); dataInicio
+
 
         $lancamentosAll = Lancamento::selectAll();
 
+        if ($request->has('dataInicio', 'dataFim', 'status')) {
+            $dataInicio = $request->dataInicio;
+            $dataFim = $request->dataFim;
+            $status = $request->status;
+            dd($dataInicio, $dataFim, $status);
 
-        // dd($lancamentosAll);
-        // $lancamentos = Lancamento::findOne($dataInicio,$dataFim);
-        // dd($lancamentos);
-       if($request->has('dataInicio','dataFim','status'))
-       {
-        $dataInicio = $request->dataInicio;
-        $dataFim = $request->dataFim;
-        $status = $request->status;
-        dd($dataInicio,$dataFim,$status);
-
-           $filtro = explode("\r\n",$campos);
-           foreach($filtro as $f ){
-               $lancamentosAll = $this->$filtro->select($f)->get();
-
-             };
-
-        }else{
+            $filtro = explode("\r\n", $campos);
+            foreach ($filtro as $f) {
+                $lancamentosAll = $this->$filtro->select($f)->get();
+            };
+        } else {
             return response()->json($lancamentosAll);
         }
-
-
-    //     $lancamento = DB::table('intranet.tab_lancamento')
-    //         ->select(DB::select('SELECT lancamento.id_tab_lancamento,
-    //     lancamento.fk_condicao_pagamento_id,
-    //     lancamento.dt_vencimento,
-    //     lancamento.dt_fim,
-    //     despesa.id_despesa,
-    //     despesa.fk_status_despesa_id,
-	//     status_dep.de_status_despesa,
-	//     despesa.de_despesa,
-    //     despesa.valor_total_despesa,
-    //     despesa.dt_vencimento,
-    //     despesa.dt_provisionamento,
-    //     pagamento.fk_tab_lancamento_id,
-    //     pagamento.de_pagamento
-    //     FROM intranet.tab_lancamento AS lancamento
-    //     RIGHT JOIN intranet.tab_despesa as despesa on (lancamento.fk_tab_despesa_id = despesa.id_despesa)
-    //     LEFT JOIN intranet.tab_pagamento as pagamento on (despesa.id_despesa = pagamento.fk_tab_lancamento_id)
-    //     inner join intranet.status_despesa as status_dep on (despesa.fk_status_despesa_id = status_dep.id_status_despesa)
-    //     LIMIT 10'));
-
-    //  $lancamento = DB::table('intranet.tab_lancamento');
-    //  dd($lancamento);
 
     }
 
