97ba5eb29394da6cfe2f139de41eb8b6f49751e8
Wagner Silva <wagnercodeandgo@gmail.com>
2022-01-17 12:31:21 -0300

    Teste do funcionamento da DespesaRepository

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\UF;
 use App\Models\Cidade;
 use Illuminate\Http\Request;
+use App\Repository\DespesaRepository;
 
 
 class TesteController extends Controller
@@ -14,12 +15,16 @@ class TesteController extends Controller
     public function all()
     {
 
-        $uf = new UF();
-        $cidade = new Cidade();
+        // $uf = new UF();
+        // $cidade = new Cidade();
 
-        $uf = $uf::findIdByUF('MG');
-        $cidade = $cidade::findIdByCidade('BELO HORIZONTE');
+        // $uf = $uf::findIdByUF('MG');
+        // $cidade = $cidade::findIdByCidade('BELO HORIZONTE');
 
-        dd($uf, $cidade);
+        // dd($uf, $cidade);
+
+        $despesaRepository = new DespesaRepository();
+        $test = $despesaRepository->findPaymentCondition("18164");
+        dd($test[0]->fk_condicao_pagamento_id);
     }
 }
