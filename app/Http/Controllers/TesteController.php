92321c0ff2ab31ebfda8715c068fdd8f74798c16
Wagner Silva <wagnercodeandgo@gmail.com>
2022-01-20 15:46:03 -0300

    teste data lancamento

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\UF;
 use App\Models\Cidade;
+use App\Models\Extrato;
 use Illuminate\Http\Request;
 use App\Repository\DespesaRepository;
 use App\Models\Lancamento;
@@ -27,7 +28,9 @@ class TesteController extends Controller
         $despesaRepository = new DespesaRepository();
         $test = $despesaRepository->findInfosDespesa("18164");
         
-        $timeStamp = Lancamento::findIdByTimeStamp('2022-01-17 12:11:56');
-        dd($timeStamp[0]->id_tab_lancamento);
+        // $timeStamp = Lancamento::findIdByTimeStamp('2022-01-17 12:11:56');
+        $test = Lancamento::findByPeriod('2022-01-01','2022-01-20');
+        dd($test);
+        // dd($timeStamp[0]->id_tab_lancamento);
     }
 }
