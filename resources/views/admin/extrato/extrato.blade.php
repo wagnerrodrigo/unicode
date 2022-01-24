1e888992cf280ec3767da785ea5dfc929221828f
Pedroenrick <pedro_penna22@hotmail.com>
2022-01-24 17:07:10 -0300

    modificando titulo da tela de extratos

--- resources/views/admin/extrato/extrato.blade.php
+++ resources/views/admin/extrato/extrato.blade.php
@@ -7,7 +7,7 @@
 
         <div class="card">
             <div class="card-header">
-                <h1>DESPESA </h1>
+                <h1>LANÇAMENTOS DISPONIVEIS PARA CONCILIAÇÃO </h1>
             </div>
             <div class="card-body">
                 <form action="{{ route('extrato') }}" method="GET">
