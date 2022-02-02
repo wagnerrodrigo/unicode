a79fc11ba59e25f319a0c2a33c12c183ba0817da
Wagner Silva <wagnercodeandgo@gmail.com>
2022-02-02 10:22:43 -0300

    Adicionado novo campo Data do Efetivo Pagamento

--- app/Models/Lancamento.php
+++ app/Models/Lancamento.php
@@ -169,14 +169,16 @@ class Lancamento extends Model
             dt_lancamento,
             dt_vencimento,
             dt_inicio,
+            dt_efetivo_pagamento,
             dt_fim
         )
-        VALUES(?, ?, ?, ?, ?, ?)", [
+        VALUES(?, ?, ?, ?, ?, ?, ?)", [
             $lancamento->id_despesa,
             $lancamento->fk_condicao_pagamento_id,
             $lancamento->dt_lancamento,
             $lancamento->dt_vencimento,
             $lancamento->dt_inicio,
+            $lancamento->dt_efetivo_pagamento,
             $lancamento->dt_fim
         ]);
     }
