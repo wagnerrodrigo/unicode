7a09202c8c0e218245d9aa2fef37a51236ad0d9f
Pedroenrick <pedro_penna22@hotmail.com>
2022-02-18 09:16:42 -0300

    [FIX] modificando favicon login

--- resources/views/layouts/templates/template-login.blade.php
+++ resources/views/layouts/templates/template-login.blade.php
@@ -8,7 +8,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
-    <link rel="shortcut icon" href="{{asset('assets/images/favicon.svg')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('assets/images/LOGO/favicon.svg')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
     <link rel="stylesheet" href="{{asset('assets/vendors/perfect-scrollbar/perfect-scrollbar.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
