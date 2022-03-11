32ae97eac678ac83bfaee5c57fceedcecec5871b
Pedroenrick <pedro_penna22@hotmail.com>
2022-03-11 17:13:42 -0300

    [FEATURE] adicionando validação ao controller de pagamentos

--- app/Http/Controllers/PagamentoController.php
+++ app/Http/Controllers/PagamentoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Utils\Mascaras\Mascaras;
 use App\Repository\RateioRepository;
 use App\Repository\DespesaRepository;
+use App\CustomError\CustomErrorMessage;
 
 class PagamentoController extends Controller
 {
@@ -17,54 +18,62 @@ class PagamentoController extends Controller
      */
     public function index(Request $request)
     {
-        $filtros = null;
+        try {
+            $filtros = null;
 
-        if ($request->has('results') || $request->has('dt_inicio') && $request->has('dt_fim')) {
-            $filtros = [
-                'resultado_por_pagina' => $request->input('results') == null ? 10 : $request->input('results'),
-                'dt_inicio_periodo' => $request->input('dt_inicio'),
-                'dt_fim_periodo' => $request->input('dt_fim'),
-                'status_despesa_id' => $request->input('status')
-            ];
+            if ($request->has('results') || $request->has('dt_inicio') && $request->has('dt_fim')) {
+                $filtros = [
+                    'resultado_por_pagina' => $request->input('results') == null ? 10 : $request->input('results'),
+                    'dt_inicio_periodo' => $request->input('dt_inicio'),
+                    'dt_fim_periodo' => $request->input('dt_fim'),
+                    'status_despesa_id' => $request->input('status')
+                ];
 
-            $pagamentos = Pagamento::selectAll($filtros['resultado_por_pagina'], $filtros['dt_inicio_periodo'], $filtros['dt_fim_periodo']);
-        } else {
-            $pagamentos = Pagamento::selectAll(10);
+                $pagamentos = Pagamento::selectAll($filtros['resultado_por_pagina'], $filtros['dt_inicio_periodo'], $filtros['dt_fim_periodo']);
+            } else {
+                $pagamentos = Pagamento::selectAll(10);
+            }
+
+            $mascara = new Mascaras();
+
+            return view('admin.pagamento.lista-pagamento', compact('pagamentos', 'mascara', 'filtros'));
+        } catch (\Exception $e) {
+            $error = CustomErrorMessage::ERROR_LIST_PAGAMENTO;
+            return view('error', compact('error'));
         }
-
-        $mascara = new Mascaras();
-
-        return view('admin.pagamento.lista-pagamento', compact('pagamentos', 'mascara', 'filtros'));
     }
-
 
     public function show($id)
     {
+        try {
+            $mascara = new Mascaras();
+            //pega o pagamento
+            $pagamento = Pagamento::findOne($id);
 
-        $mascara = new Mascaras();
-        //pega o pagamento
-        $pagamento = Pagamento::findOne($id);
+            foreach ($pagamento as $pagamento) {
+            }
 
-        foreach ($pagamento as $pagamento) {
+            //busca o tipo de despesa
+            $despesaRepository = new DespesaRepository();
+            $tipoDaDespesa = $despesaRepository->findInfosDespesa($pagamento->fk_tab_despesa_id);
+
+            foreach ($tipoDaDespesa as $tipoDaDespesa) {
+            }
+
+            $pagamentos = Pagamento::getInfosPagamento($id, $tipoDaDespesa->fk_tab_tipo_despesa_id);
+
+            foreach ($pagamentos as $pagamento) {
+            }
+
+            //pega os rateios
+            $rateioRepository = new RateioRepository();
+            $rateios = $rateioRepository->findRateioDespesa($pagamento->id_despesa);
+
+            return view('admin.pagamento.despesa-paga', compact('pagamento', 'mascara', 'rateios'));
+        } catch (\Exception $e) {
+            $error = CustomErrorMessage::ERROR_PAGAMENTO;
+            return view('error', compact('error'));
         }
-
-        //busca o tipo de despesa
-        $despesaRepository = new DespesaRepository();
-        $tipoDaDespesa = $despesaRepository->findInfosDespesa($pagamento->fk_tab_despesa_id);
-
-        foreach ($tipoDaDespesa as $tipoDaDespesa) {
-        }
-
-        $pagamentos = Pagamento::getInfosPagamento($id, $tipoDaDespesa->fk_tab_tipo_despesa_id);
-
-        foreach ($pagamentos as $pagamento) {
-        }
-
-        //pega os rateios
-        $rateioRepository = new RateioRepository();
-        $rateios = $rateioRepository->findRateioDespesa($pagamento->id_despesa);
-
-        return view('admin.pagamento.despesa-paga', compact('pagamento', 'mascara', 'rateios'));
     }
 
 
