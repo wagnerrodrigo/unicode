09006285d64a119e05e14c7e23732b47986944c0
Pedroenrick <pedro_penna22@hotmail.com>
2022-03-29 14:22:31 -0300

    [FIX] modificando nome do middleware de verificacao de login

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'autenticacaoMiddleware' => \App\Http\Middleware\UserAuthenticate::class
+        'autenticacaoMiddleware' => \App\Http\Middleware\AuthenticatedUser::class
     ];
 }
