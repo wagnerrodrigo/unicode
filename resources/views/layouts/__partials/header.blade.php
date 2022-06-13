c6d3d25be7aec9a418f20ddc385ec6bf3176c9ed
Pedroenrick <pedro_penna22@hotmail.com>
2022-06-13 11:24:34 -0300

    [fix] removendo links de acesso ao modulo de compras

--- resources/views/layouts/__partials/header.blade.php
+++ resources/views/layouts/__partials/header.blade.php
@@ -21,14 +21,14 @@
                     </ul>
                 </li>
 
-                <li class="nav-item dropdown space-item-navbar" style="padding: 10px;">
+                <!-- <li class="nav-item dropdown space-item-navbar" style="padding: 10px;">
                     <a class="nav-link dropdown-toggle" href="/#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         COMPRAS
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="compras">
                         <li><a class="dropdown-item" href="{{route('compras')}}">COMPRAS</a></li>
                     </ul>
-                </li>
+                </li> -->
             </ul>
             <!-- Left links -->
         </div>
